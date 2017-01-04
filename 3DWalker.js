14f8a154ce1923e91527c8f0f1b61b4c798f6ce1
Yang Liu <myly10@gmail.com>
2017-01-05 05:02:52 +0800

    Fully Finished

--- 3DWalker.js
+++ 3DWalker.js
@@ -4,8 +4,15 @@ let gl,
 	viewProjMatrix = new Matrix4()
 	;
 
+let status={
+	lightOn: false,
+	locked: false,
+	rotX: 0,
+	rotY: 0
+};
+
 function main() {
-	gl=assert(getWebGLContext(canvas, false), 'Failed to get the rendering context for WebGL');
+	gl=assert(getWebGLContext(canvas, !false), 'Failed to get the rendering context for WebGL');
 
 	let objProgram=assert(createProgram(gl, OBJECT_VSHADER_SRC, OBJECT_FSHADER_SRC), 'Failed to intialize shaders.');
 	objProgram.a_Position = gl.getAttribLocation(objProgram, 'a_Position');
@@ -19,7 +26,7 @@ function main() {
 	objProgram.u_PointLightPosition = gl.getUniformLocation(objProgram, 'u_PointLightPosition');
 	if (objProgram.a_Position<0 || objProgram.a_Normal<0 || objProgram.a_Color<0 || objProgram.u_MvpMatrix<0 || objProgram.u_NormalMatrix<0 || objProgram.u_AmbientLight<0 || objProgram.u_LightDirection<0 || objProgram.u_PointLightColor<0 || objProgram.u_PointLightPosition<0)
 		throw new Error("Failed to get location");
-/*
+
 	let textureProgram=assert(createProgram(gl, TEXTURED_VSHADER_SRC, TEXTURED_FSHADER_SRC), 'Failed to intialize shaders.');
 	textureProgram.a_Position = gl.getAttribLocation(textureProgram, 'a_Position');
 	textureProgram.a_TexCoord = gl.getAttribLocation(textureProgram, 'a_TexCoord');
@@ -27,15 +34,8 @@ function main() {
 	textureProgram.u_Sampler = gl.getUniformLocation(textureProgram, 'u_Sampler');
 	if (textureProgram.a_Position<0 || textureProgram.a_TexCoord<0 || textureProgram.u_MvpMatrix<0 || textureProgram.u_Sampler<0)
 		throw new Error("Failed to get location");
-*/
-	gl.clearColor(0.0, 0.0, 0.0, 1.0);
 
-	viewProjMatrix.setPerspective(CameraPara.fov, canvas.width/canvas.height, CameraPara.near, CameraPara.far);
-	viewProjMatrix.lookAt(
-		CameraPara.eye[0], CameraPara.eye[1], CameraPara.eye[2],
-		CameraPara.at[0],  CameraPara.at[1],  CameraPara.at[2],
-		CameraPara.up[0],  CameraPara.up[1],  CameraPara.up[2]
-	);
+	gl.clearColor(0.0, 0.0, 0.0, 1.0);
 
 	for (let obj of ObjectList){
 		obj.objdoc=null;
@@ -44,19 +44,46 @@ function main() {
 		readOBJFile(obj.objFilePath, obj, 1, true);
 	}
 
+	floorRes.texture=initTexture(gl, textureProgram, floorRes.texImagePath);
+	floorRes=initAndFeedTextureObjBuffers(gl, floorRes);
+	boxRes.texture=initTexture(gl, textureProgram, boxRes.texImagePath);
+	boxRes=initAndFeedTextureObjBuffers(gl, boxRes);
+
 	let currentAngle = 0.0; // Current rotation angle [degree]
 	let tick = function() {   // Start drawing
-		currentAngle = animate(currentAngle); // Update current rotation angle
+		viewProjMatrix.setPerspective(CameraPara.fov, canvas.width/canvas.height, CameraPara.near, CameraPara.far);
+		viewProjMatrix.rotate(status.rotX, 0, 1, 0);
+		viewProjMatrix.rotate(status.rotY, 1, 0, 0);
+		viewProjMatrix.lookAt(
+			CameraPara.eye[0], CameraPara.eye[1], CameraPara.eye[2],
+			CameraPara.at[0],  CameraPara.at[1],  CameraPara.at[2],
+			CameraPara.up[0],  CameraPara.up[1],  CameraPara.up[2]
+		);
 
-		gl.useProgram(objProgram);
+		currentAngle = animate(currentAngle); // Update current rotation angle
 		gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);  // Clear color and depth buffers
 		gl.enable(gl.DEPTH_TEST);
-		for (let obj of ObjectList){
-			drawObj(gl, objProgram, 0, viewProjMatrix, obj);
+
+		gl.useProgram(objProgram);
+		for (let obj of ObjectList) {
+			if (obj.objFilePath.includes('gumby'))
+				drawObj(gl, objProgram, currentAngle, viewProjMatrix, obj);
+			else
+				drawObj(gl, objProgram, 0, viewProjMatrix, obj);
 		}
+
+		gl.useProgram(textureProgram);
+		drawTextureObj(gl, textureProgram, floorRes);
+		drawTextureObj(gl, textureProgram, boxRes);
+
 		requestAnimationFrame(tick);
 	};
 	tick();
+
+	canvas.addEventListener('click', function(){canvas.requestPointerLock();}, false);
+	canvas.addEventListener('mousemove', mouseMoveHandler, false);
+	document.addEventListener('pointerlockchange', pointerLockHandler, false);
+	document.onkeydown=keyDownHandler;
 }
 
 function initVertexBuffers(gl, program) {
@@ -137,11 +164,11 @@ function drawObj(gl, objProgram, angle, viewProjMatrix, obj) {
 	gl.uniform3fv(objProgram.u_LightDirection, lightDirection.elements);
 	gl.uniform3fv(objProgram.u_AmbientLight, new Float32Array(sceneAmbientLight));
 	gl.uniform3f(objProgram.u_PointLightPosition, CameraPara.eye[0], CameraPara.eye[1], CameraPara.eye[2]);
+	if (status.lightOn) gl.uniform3f(objProgram.u_PointLightColor, 0.2, 0.2, 0.2);
+	else gl.uniform3f(objProgram.u_PointLightColor, 0,0,0);
 
 	g_modelMatrix.setIdentity();
-	g_modelMatrix.setRotate(angle, 1.0, 0.0, 0.0); // 適当に回転
-	g_modelMatrix.rotate(angle, 0.0, 1.0, 0.0);
-	g_modelMatrix.rotate(angle, 0.0, 0.0, 1.0);
+	g_modelMatrix.setRotate(angle, 0.0, 1.0, 0.0); // 適当に回転
 
 	for (let entry of obj.transform) {
 		if (entry.type == 'translate')
@@ -209,4 +236,123 @@ function initAttributeVariable(gl, attribPtr, buffer) {
 	gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
 	gl.vertexAttribPointer(attribPtr, buffer.num, buffer.type, false, 0, 0);
 	gl.enableVertexAttribArray(attribPtr);
+}
+
+function initTexture(gl, program, imageUrl) {
+	var texture = gl.createTexture();   // Create a texture object
+	if (!texture) {
+		console.log('Failed to create the texture object');
+		return null;
+	}
+
+	var image = new Image();  // Create a image object
+	if (!image) {
+		console.log('Failed to create the image object');
+		return null;
+	}
+	// Register the event handler to be called when image loading is completed
+	image.onload = function() {
+		// Write the image data to texture object
+		gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, 1);  // Flip the image Y coordinate
+		gl.activeTexture(gl.TEXTURE0);
+		gl.bindTexture(gl.TEXTURE_2D, texture);
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+		gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, image);
+
+		// Pass the texure unit 0 to u_Sampler
+		gl.useProgram(program);
+		gl.uniform1i(program.u_Sampler, 0);
+
+		gl.bindTexture(gl.TEXTURE_2D, null); // Unbind texture
+	};
+
+	image.src = imageUrl;
+
+	return texture;
+}
+
+function initAndFeedTextureObjBuffers(gl, texObj){
+	texObj.vertexBuffer = assert(gl.createBuffer(), 'Failed to create the buffer object');
+	gl.bindBuffer(gl.ARRAY_BUFFER, texObj.vertexBuffer);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(texObj.vertex), gl.STATIC_DRAW);
+	texObj.vertexBuffer.type=gl.FLOAT;
+	texObj.vertexBuffer.num=3;
+
+	texObj.texCoordBuffer = assert(gl.createBuffer(), 'Failed to create the buffer object');
+	gl.bindBuffer(gl.ARRAY_BUFFER, texObj.texCoordBuffer);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(texObj.texCoord), gl.STATIC_DRAW);
+	texObj.texCoordBuffer.type=gl.FLOAT;
+	texObj.texCoordBuffer.num=2;
+
+	let uint8IndexArr = new Uint8Array(texObj.index);
+	texObj.indexBuffer = assert(gl.createBuffer(), 'Failed to create the buffer object');
+	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, texObj.indexBuffer);
+	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, uint8IndexArr, gl.STATIC_DRAW);
+	texObj.indexBuffer.type=gl.UNSIGNED_BYTE;
+
+	if (!texObj.vertexBuffer || !texObj.texCoordBuffer || !texObj.indexBuffer) return null;
+	texObj.numIndices = uint8IndexArr.length;
+
+	gl.bindBuffer(gl.ARRAY_BUFFER, null);
+	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+	return texObj;
+}
+
+function drawTextureObj(gl, texProgram, obj){
+	let modelMatrix=new Matrix4(), mvpMatrix=new Matrix4();
+	modelMatrix.setTranslate(obj.translate[0], obj.translate[1], obj.translate[2]);
+	modelMatrix.scale(obj.scale[0], obj.scale[1], obj.scale[2]);
+
+	mvpMatrix.set(viewProjMatrix).multiply(modelMatrix);
+	gl.uniformMatrix4fv(texProgram.u_MvpMatrix, false, mvpMatrix.elements);
+
+	initAttributeVariable(gl, texProgram.a_Position, obj.vertexBuffer);
+	initAttributeVariable(gl, texProgram.a_TexCoord, obj.texCoordBuffer);
+
+	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, obj.indexBuffer);
+	gl.activeTexture(gl.TEXTURE0);
+	gl.bindTexture(gl.TEXTURE_2D, obj.texture);
+	gl.uniform1i(texProgram.u_Sampler, 0);
+	gl.drawElements(gl.TRIANGLES, obj.numIndices, obj.indexBuffer.type, 0);
+}
+
+function mouseMoveHandler(ev) {
+	if (status.locked) {
+		status.rotX += ev.movementX*0.2;
+		status.rotY += ev.movementY*0.2;
+		if (status.rotX<-360 || status.rotX>360) status.rotX%=360;
+		if (status.rotY<-85) status.rotY=-85;
+		if (status.rotY>85) status.rotY=85;
+		console.log(status.rotX+' '+status.rotY);
+	}
+}
+
+function keyDownHandler(ev) {
+	let keycodeChar=String.fromCharCode(ev.keyCode);
+	let elps=20;
+	switch (keycodeChar){
+		case 'W':
+			CameraPara.eye[2]-=MOVE_VELOCITY*elps/1000;
+			CameraPara.at[2]-=MOVE_VELOCITY*elps/1000*MOVE_VELOCITY;
+			break;
+		case 'A':
+			CameraPara.eye[0]-=MOVE_VELOCITY*elps/1000;
+			CameraPara.at[0]-=MOVE_VELOCITY*elps/1000;
+			break;
+		case 'S':
+			CameraPara.eye[2]+=MOVE_VELOCITY*elps/1000;
+			CameraPara.at[2]+=MOVE_VELOCITY*elps/1000;
+			break;
+		case 'D':
+			CameraPara.eye[0]+=MOVE_VELOCITY*elps/1000;
+			CameraPara.at[0]+=MOVE_VELOCITY*elps/1000;
+			break;
+		case 'F':
+			status.lightOn=!status.lightOn;
+			break;
+	}
+}
+
+function pointerLockHandler(ev) {
+	status.locked=true;
 }
